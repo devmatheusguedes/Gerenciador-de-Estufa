93d2765bd9edb79a3a41b2ab41049da1490a64a8
matheus guedes <guedesmatheus133@gmail.com>
2025-12-19 23:39:59 -0300

    create tela estufas 1.0

---
+++ app/src/main/java/br/com/gerenciadordeestufa/EstufaActivity.java
@@ -0,0 +1,14 @@
+package br.com.gerenciadordeestufa;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EstufaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estufa);
+    }
+}
