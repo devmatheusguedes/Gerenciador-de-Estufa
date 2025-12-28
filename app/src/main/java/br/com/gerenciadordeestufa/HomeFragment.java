7a20dda0216339f0538dbcc263b87e30da37ba94
matheus guedes <guedesmatheus133@gmail.com>
2025-12-27 22:09:55 -0300

    add navigation in HomeFragment

--- app/src/main/java/br/com/gerenciadordeestufa/HomeFragment.java
+++ app/src/main/java/br/com/gerenciadordeestufa/HomeFragment.java
@@ -9,17 +9,31 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 
 public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        View cardEstufas = view.findViewById(R.id.card_estufas_id);
-        cardEstufas.setOnClickListener(v ->{
-            NavHostFragment
-                }
-        );
+        configurarCliques(view);
+    }
+
+    private void configurarCliques(View view) {
+        view.findViewById(R.id.card_estufas_id).setOnClickListener(v -> navegar(R.id.action_homeFragment_to_estufasFragment2));
+        view.findViewById(R.id.card_caixas_dagua_id).setOnClickListener(v -> navegar(R.id.action_homeFragment_to_caixaDaguaFragment));
+        view.findViewById(R.id.btn_new_reading).setOnClickListener(v -> navegar(R.id.action_homeFragment_to_novaleituraFragment));
+        view.findViewById(R.id.card_alertas_id).setOnClickListener(v -> navegar(R.id.action_homeFragment_to_alertaFragment));
+    }
+
+    private void navegar(int actionId) {
+        NavHostFragment.findNavController(this).navigate(actionId);
     }
 }
