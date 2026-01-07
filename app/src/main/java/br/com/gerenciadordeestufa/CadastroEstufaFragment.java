5b9d01ef924c1bc90a8d1c1af3c07962b9022d54
matheus guedes <guedesmatheus145@gmail.com>
2026-01-06 21:16:42 -0300

    add fragment cadastro estufa

--- app/src/main/java/br/com/gerenciadordeestufa/CadastroEstufaFragment.java
+++ app/src/main/java/br/com/gerenciadordeestufa/CadastroEstufaFragment.java
@@ -10,17 +10,23 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class EstufasFragment extends Fragment {
+public class CadastroEstufaFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_estufa, container, false);
+        View view = inflater.inflate(R.layout.fragment_cadastro_estufa, container, false);
 
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
     private void configurarCliques(View view) {
-        view.findViewById(R.id.btn_add_greenhouse).setOnClickListener(v -> navegar(R.id.action_estufasFragment2_to_cadastroEstufaFragment));
+
     }
 
     private void navegar(int actionId) {
