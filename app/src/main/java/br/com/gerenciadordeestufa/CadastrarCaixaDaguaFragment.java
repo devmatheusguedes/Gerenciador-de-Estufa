bf4d8ec2d33001a155c02902f1b66f085ae23832
matheus guedes <guedesmatheus145@gmail.com>
2026-01-09 10:08:27 -0300

    add class CadastroCaixaDaguaFragment and fragment_cadastroCaixa_dagua

---
+++ app/src/main/java/br/com/gerenciadordeestufa/CadastrarCaixaDaguaFragment.java
@@ -0,0 +1,24 @@
+package br.com.gerenciadordeestufa;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CadastrarCaixaDaguaFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.onCreateView(R.layout.fragment_cadastro_caixa_dagua, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
